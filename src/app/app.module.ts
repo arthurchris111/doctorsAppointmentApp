f1844fedc1379d9a95f3bb5821d0617fdd55486f
christian <arthurchristian111@gmail.com>
2023-07-13 16:01:29 +0000

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { PartialsComponent } from './authentication/partials/partials.component';
+import { LoginComponent } from './authentication/partials/login/login.component';
+import { SignupComponent } from './authentication/partials/signup/signup.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent
+    AuthenticationComponent,
+    PartialsComponent,
+    LoginComponent,
+    SignupComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PagesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule],
+})
+export class PagesModule {}
