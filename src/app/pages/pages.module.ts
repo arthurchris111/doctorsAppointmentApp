7d6a816653007e297716b6ac913bcec53023fcf6
christian <arthurchristian111@gmail.com>
2023-07-13 23:51:36 +0000

    hero image and text

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
-import { HomeComponent } from './home/home.component';
 import { LayoutModule } from '../layout/layout.module';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, PagesRoutingModule, LayoutModule],
