2b35f9ff3dc78b88a7114047cc5d369f4758b537
christian <arthurchristian111@gmail.com>
2023-07-18 23:58:28 +0000

    changes

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,8 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './partials/login/login.component';
 import { SignupComponent } from './partials/signup/signup.component';
 import { AuthenticationComponent } from './authentication.component';
+import { LayoutModule } from '../layout/layout.module';
 @NgModule({
   declarations: [LoginComponent, SignupComponent, AuthenticationComponent],
-  imports: [CommonModule, AuthenticationRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    ReactiveFormsModule,
+    LayoutModule,
+  ],
 })
 export class AuthenticationModule {}
--- src/app/authentication/partials/login/login.component.ts
+++ src/app/authentication/partials/login/login.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  login!: FormGroup;
+  show: boolean = false;
+  submitted: boolean = false;
+  isFetching: boolean = false;
+  user: boolean = false;
+  password: any;
 
+  constructor(private formBuilder: FormBuilder, private route: Router) {}
+
+  buildLoginForm(): void {
+    this.login = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      rememberMe: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.buildLoginForm();
+    this.password = 'password';
+  }
+
+  togglePassword() {
+    this.show = !this.show;
+    console.log(this.show);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.isFetching = true;
+
+    if (this.login.invalid) {
+      return;
+    }
+
+    console.log(this.login.value);
+  }
 }
