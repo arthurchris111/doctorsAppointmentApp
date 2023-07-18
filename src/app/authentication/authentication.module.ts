34cd7eb56b2e4eb78c34a5b937b4577849a53b65
christian <arthurchristian111@gmail.com>
2023-07-18 21:45:40 +0000

    changes

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,14 +4,9 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './partials/login/login.component';
 import { SignupComponent } from './partials/signup/signup.component';
-import { LayoutModule } from '../layout/layout.module';
+import { AuthenticationComponent } from './authentication.component';
 @NgModule({
-  declarations: [LoginComponent, SignupComponent],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule,
-    ReactiveFormsModule,
-    LayoutModule,
-  ],
+  declarations: [LoginComponent, SignupComponent, AuthenticationComponent],
+  imports: [CommonModule, AuthenticationRoutingModule, ReactiveFormsModule],
 })
 export class AuthenticationModule {}
