9be986711c8ab4ee895828764b3ab9d13c91befe
Christian <arthurchristian111@gmail.com>
2023-10-03 23:53:13 +0300

    changes

--- src/app/authentication/partials/login/login.component.ts
+++ src/app/authentication/partials/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { services } from 'src/app/service/services';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,11 @@ export class LoginComponent {
   user: boolean = false;
   password: any;
 
-  constructor(private formBuilder: FormBuilder, private route: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private service: services
+  ) {}
 
   buildLoginForm(): void {
     this.login = this.formBuilder.group({
@@ -40,6 +45,32 @@ export class LoginComponent {
   onSubmit() {
     this.submitted = true;
     this.isFetching = true;
+
+    this.service.loginDetails().subscribe({
+      next: (responseData: any) => {
+        this.isFetching = false;
+        const userArray = [];
+
+        for (const key in responseData) {
+          console.log(responseData[key]);
+          userArray.push(responseData[key]);
+        }
+
+        const user = userArray.find((result: any) => {
+          return (
+            result.email === this.login.value.email &&
+            result.password === this.login.value.password
+          );
+        });
+
+        if (user) {
+          this.route.navigate(['home']);
+          alert('Login Successfully');
+        } else {
+          alert('user not found');
+        }
+      },
+    });
 
     if (this.login.invalid) {
       return;
