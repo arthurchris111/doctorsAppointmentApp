54e6159f5baa01105da4b024526526527523e657
Christian <arthurchristian111@gmail.com>
2023-10-09 19:54:06 +0000

    fire

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -7,6 +7,7 @@ import { SignupComponent } from './partials/signup/signup.component';
 import { AuthenticationComponent } from './authentication.component';
 import { LayoutModule } from '../layout/layout.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
 @NgModule({
   declarations: [LoginComponent, SignupComponent, AuthenticationComponent],
   imports: [
@@ -15,6 +16,15 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     LayoutModule,
     HttpClientModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDxV9hEBX1e4kRrdyqBomyL0TT0wYTwpP4',
+      authDomain: 'doctor-s-appointments.firebaseapp.com',
+      databaseURL: 'https://doctor-s-appointments-default-rtdb.firebaseio.com',
+      projectId: 'doctor-s-appointments',
+      storageBucket: 'doctor-s-appointments.appspot.com',
+      messagingSenderId: '672588399008',
+      appId: '1:672588399008:web:2ee5a3750d46bd5b4c64c5',
+    }),
   ],
 })
 export class AuthenticationModule {}
